95a910dba28ab95ff418d18006f616400dd86ccc
Kayla McNeeley <mcneelek@oregonstate.edu>
2017-10-02 09:50:01 -0700

    Draw 2 circles

--- sketch.js
+++ sketch.js
@@ -1,1 +1,15 @@
 // zombulator by Kayla McNeeley
+
+function setup() {
+	createCanvas(800, 800);
+
+}
+
+function draw() {
+	fill(255, 0, 0);
+	ellipse(50, 50, 80, 80);
+	fill(150, 150, 200);
+	ellipse(200, 100, 80, 80)
+
+}
+
