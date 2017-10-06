12ccf12c7b68b87a380e05f895c4e8672a959f8b
Kayla McNeeley <mcneelek@oregonstate.edu>
2017-10-06 09:56:04 -0700

    zombieX bouce, boarder around circles

--- sketch.js
+++ sketch.js
@@ -1,15 +1,24 @@
 // zombulator by Kayla McNeeley
 
+var zombieX = 50;
+var zombieY = 100;
+
 function setup() {
 	createCanvas(800, 800);
-
 }
 
 function draw() {
+	background(255, 255, 255);
 	fill(255, 0, 0);
-	ellipse(50, 50, 80, 80);
+	strokeWeight(4);
+	stroke(0, 204, 204);
+	ellipse(zombieX, 50, 80, 80);
 	fill(150, 150, 200);
-	ellipse(200, 100, 80, 80)
-
+	ellipse(zombieY, 100, 80, 80)
+	zombieX = zombieX + 1;
+	zombieY = zombieY + 3;
+	if (zombieX >= 800) {
+		zombieX= 0;
+	}
 }
 
