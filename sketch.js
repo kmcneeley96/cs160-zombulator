6f1fd3d81338ef71e69627106a29fdd66062a119
Kayla McNeeley <mcneelek@oregonstate.edu>
2017-10-08 17:33:26 -0700

    Changed zombieX color, zombieY color and, stroke color.

--- sketch.js
+++ sketch.js
@@ -9,16 +9,19 @@ function setup() {
 
 function draw() {
 	background(255, 255, 255);
-	fill(255, 0, 0);
-	strokeWeight(4);
-	stroke(0, 204, 204);
+	fill(17, 4, 92);
+	strokeWeight(9);
+	stroke(247, 211, 9);
 	ellipse(zombieX, 50, 80, 80);
-	fill(150, 150, 200);
+	fill(158, 72, 6);
 	ellipse(zombieY, 100, 80, 80)
 	zombieX = zombieX + 1;
 	zombieY = zombieY + 3;
 	if (zombieX >= 800) {
 		zombieX= 0;
+	}
+	if (zombieY >= 300) {
+		zombieY= 0;
 	}
 }
 
