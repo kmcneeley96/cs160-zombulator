fb694a8597bec32c37c5cfbb908c495da82cac85
Kayla McNeeley <mcneelek@oregonstate.edu>
2017-11-06 10:00:26 -0800

    EX 19 complete displaying Z and H count

--- sketch.js
+++ sketch.js
@@ -1,41 +1,62 @@
-// http://tinyurl.com/cs160ex19
-// Zombulator by Kayla McNeeley
-// CS 160 Exercise 19: Polymorphism
+//tinyurl.com/cs160ex20
+// Zombulator by YOUR NAME
+// CS 160 Exercise 20: Collisions
 
 var backgroundColor;
 
 const MIN_SIZE = 5;
 const MAX_SIZE = 50;
-const NUMBER_OF_ZOMBIES = 100;
-const NUMBER_OF_HUMANS = 100;
+const POPULATION_SIZE = 500;
 
-var zombies;
+var population = [];
 
-var humans;
+var zombieCount = 0;
+var humanCount = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   backgroundColor = color(245, 255, 245);
-  initializeZombies();
-  initializeHumans();
+  initializePopulation();
 }
 
 function draw() {
   background(backgroundColor);
   noStroke();
-  drawZombies();
-  moveZombies();
-  drawHumans();
-  moveHumans();
+  drawPopulation();
+  movePopulation();
+  drawPopulationCounts();
 }
 
+function initializePopulation() {
+  for (var i = 0; i < POPULATION_SIZE; ++i) {
+    var humanoid_type = random(0, 100);
+    if (humanoid_type <= 50) {
+      population[i] = initializeZombie();
+      ++zombieCount;
+    } else {
+      population[i] = initializeHuman();
+     ++humanCount;
+    }
+  }
+}
 
-// Zombies. Raaahh!
+function drawPopulationCounts() {
+  stroke(0);
+  textSize(72);
+  textAlign(CENTER);
+  text("Zombies: " + zombieCount, width / 2, 100);
+  text("Humans: " + humanCount, width / 2, height - 100);
+}
 
-function initializeZombies() {
-  zombies = [];
-  for (var i = 0; i < NUMBER_OF_ZOMBIES; ++i) {
-    zombies[i] = initializeZombie();
+function drawPopulation() {
+  for (var i = 0; i < POPULATION_SIZE; ++i) {
+    population[i].draw();
+  }
+}
+
+function movePopulation() {
+  for (var i = 0; i < POPULATION_SIZE; ++i) {
+    population[i].move();
   }
 }
 
@@ -65,61 +86,30 @@ function initializeZombie() {
   };
 }
 
-function drawZombies() {
-  for (var i = 0; i < NUMBER_OF_ZOMBIES; ++i) {
-    zombies[i].draw();
-  }
-}
-
-function moveZombies() {
-  for (var i = 0; i < NUMBER_OF_ZOMBIES; ++i) {
-    zombies[i].move();
-  }
-}
-
-// Humans. Mmmm brains!
-
-function initializeHumans() {
-  humans = [];
-  for (var i = 0; i < NUMBER_OF_HUMANS; ++i) {
-    humans[i] = initializeHuman();
-  }
-}
-
-function initializeHuman(index) {
+function initializeHuman() {
   return {
     x: random(0, windowWidth),
     y: random(windowHeight - 200, windowHeight),
     speed: random(0.25, 3),
     size: random(MIN_SIZE, MAX_SIZE),
     color: color(random(50, 150), random(50, 150), random(150, 255), 150),
-    draw: function() {
-      fill(this.color);
-      ellipse(this.x, this.y, this.size, this.size);
-    },
     move: function() {
-      var direction = random(0, 100);
-      if (direction < 20) {
-        this.x += this.speed;
-      } else if (direction < 40) {
-        this.x -= this.speed;
-      } else if (direction < 60) {
-        this.y += this.speed;
-      } else {
-        this.y -= this.speed;
-      }
+        var direction = random(0, 100);
+        if (direction < 20) {
+          this.x += this.speed;
+        } else if (direction < 40) {
+          this.x -= this.speed;
+        } else if (direction < 60) {
+          this.y += this.speed;
+        } else {
+          this.y -= this.speed;
+        }
+      },
+    draw: function() {
+        fill(this.color);
+        ellipse(this.x, this.y, this.size, this.size);
     }
-  }
+  };
 }
 
-function drawHumans() {
-  for (var i = 0; i < NUMBER_OF_HUMANS; ++i) {
-    humans[i].draw();
-  }
-}
 
-function moveHumans() {
-  for (var i = 0; i < NUMBER_OF_HUMANS; ++i) {
-    humans[i].move();
-  }
-}
