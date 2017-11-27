07893284fb68c72a58b2df163c5277f91f9a7805
Kayla McNeeley <mcneelek@oregonstate.edu>
2017-11-27 08:56:57 -0800

    Moved the pop. counts far L +R & changed color to white, added grass

--- sketch.js
+++ sketch.js
@@ -6,12 +6,20 @@ var backgroundColor;
 
 const MIN_SIZE = 5;
 const MAX_SIZE = 50;
-const POPULATION_SIZE = 300;
+const POPULATION_SIZE = 100;
 
 var population = [];
 
 var zombieCount = 0;
 var humanCount = 0;
+
+var grass;
+var kittycat;
+
+function preload() {
+  grass = loadImage ("https://i.imgur.com/BtEnsR9.jpg");
+  //kittycat = loadImage ("https://i.pinimg.com/236x/91/15/85/911585f9cd0da326bc386879144aa3f3.jpg");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -20,7 +28,7 @@ function setup() {
 }
 
 function draw() {
-  background(backgroundColor);
+  background(grass);
   noStroke();
   drawPopulation();
   movePopulation();
@@ -35,7 +43,25 @@ function handleCollisions() {
       var target = population[j];
       if (attacker.isTouching(target)) {
         print("FIGHT!");
+        fight(attacker, target);
       }
+    }
+  }
+}
+
+function fight(a,t) {
+  if (a.size >= t.size) {
+    if (a.humanoidType == "zombie")
+     {print ("Winner: Attacking Zombie");
+      t.isDead();
+    } else { 
+      print ("Winner: Attacking Human");
+    }
+  } else {
+    if (t.humanoidType == "human") {
+      print ("Winner: Tageted Zombie");
+    } else {
+      print ("Winner:Targeted Zombie");
     }
   }
 }
@@ -56,10 +82,10 @@ function initializePopulation() {
 function drawPopulationCounts() {
   stroke(0);
   textSize(72);
-  textAlign(LEFT);
-  text("Dead: " + zombieCount, width / 2, 100);
-  textAlign(RIGHT);
-  text("Living: " + humanCount, width / 2, height - 100);
+  textAlign(CENTER);
+  fill(255);
+  text("Dead: " + zombieCount, 200, 100);
+  text("Living: " + humanCount, width - 200, height - 100);
 }
 
 function drawPopulation() {
@@ -95,16 +121,21 @@ function initializeZombie() {
       }
     },
     draw: function() {
+    //if (this.health ! = 0);
       fill(this.color);
       rect(this.x, this.y, this.size, this.size);
     },
 
     isTouching: function (target) {
-      if (this.humanoidType == target.humanoidType) return false;
-      var distance = dist(this.x, this.y, target.x, target.y);
-      return distance <= (this.sixe/2 + target.size/2); 
+      if (this.humanoidType == target.humanoidType) {
+        return false;
+      } else {var distance = dist(this.x, this.y, target.x, target.y);
+      return distance <= (this.sixe/2 + target.size/2);}  
+    },
+    isDead: function (){
+      this.color = color(0, 0);
     }
-  };
+  }
 }
 
 function initializeHuman() {
@@ -136,8 +167,11 @@ function initializeHuman() {
       if (this.humanoidType == target.humanoidType) return false;
       var distance = dist(this.x, this.y, target.x, target.y);
       return distance <= (this.sixe/2 + target.size/2); 
+    },
+    isDead: function (){
+      this.color = color(0, 0);
     }
-  };
+  }
 }
 
 
